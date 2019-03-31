ba4d198303ada775eeb291126d8baf349374f3f6
Samuel Beaussant <sambeda86@gmail.com>
2019-04-01 01:15:11 +0200

    pagination de tagMessage via streamBuilder nickel + ajout CircularProgressIndicator ok

--- lib/Bloc/bloc_tags_page.dart
+++ lib/Bloc/bloc_tags_page.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:tags/Bloc/bloc_provider.dart';
+import 'package:tags/Event/events.dart';
 import 'package:tags/Models/tags.dart';
 import 'package:tags/pages/TagsPage/tags_chat.dart';
 import 'package:tags/pages/TagsPage/tags_gallery.dart';
@@ -11,11 +12,19 @@ import 'package:tags/pages/TagsPage/tags_gallery.dart';
 class BlocTagsPage extends  BlocBase {
 
   final Tags _tag;
-  Tags get tags => _tag;
-  Set<DocumentSnapshot> _snapshotTagMessageList=Set<DocumentSnapshot>();
+  List<DocumentSnapshot> _snapshotTagMessageList=List<DocumentSnapshot>();
+  DocumentSnapshot _lastDocFetched;
+  bool _edgeReached=false;
+  bool _fetching=false;
 
   //liste contient les deux pages d'un tags
   List<Widget> _listPage;
+
+
+  Tags get tags => _tag;
+  List<DocumentSnapshot> get snapshotTagMessageList => _snapshotTagMessageList;
+
+
 
   //Sert à controller la page qui sera afficher, on reçoit l'int de la page à afficher
   final StreamController<int> _numTabController = StreamController<int>.broadcast();
@@ -35,13 +44,74 @@ class BlocTagsPage extends  BlocBase {
   StreamSink<QuerySnapshot> get _listTagsPageControllerSink => _listTagsPageController.sink;
   Stream<QuerySnapshot> get listTagsPageControllerStream => _listTagsPageController.stream;
 
-  
+  final StreamController<List<DocumentSnapshot>> _listTagMessageController = StreamController<List<DocumentSnapshot>>.broadcast();
+
+  StreamSink<List<DocumentSnapshot>> get _listTagMessageControllerSink => _listTagMessageController.sink;
+  Stream<List<DocumentSnapshot>> get listTagMessageControllerStream => _listTagMessageController.stream;
+
+  final StreamController<FetchMoreEvent> _fetchMoreContentController = StreamController<FetchMoreEvent>.broadcast();
+
+  StreamSink<FetchMoreEvent> get fetchMoreContentControllerSink => _fetchMoreContentController.sink;
+  Stream<FetchMoreEvent> get _fetchMoreContentControllerStream => _fetchMoreContentController.stream;
+
+
+  final StreamController<bool> _loadindController = StreamController<bool>.broadcast();
+
+  StreamSink<bool> get _loadindControllerSink => _loadindController.sink;
+  Stream<bool> get loadindControllerStream => _loadindController.stream;
+
+
+
+
+  void _fetchMoreMessage(FetchMoreEvent event) async {
+    if(_edgeReached) return;
+    _fetching=true;
+    _loadindControllerSink.add(_fetching);
+    Firestore.instance.collection("Tags").document(_tag.id)
+      .collection("TagsMessage").orderBy("id",descending:true)
+      .startAfter([_lastDocFetched.documentID]).limit(10).getDocuments().then((snap){
+        print("-*-*-*-*-*-*-*-*-*-fetching more message-*-*-*-*-*-*-*-*-*-*-*-*-*");
+        if(snap.documents.length>0) _lastDocFetched=snap.documents[snap.documents.length-1];
+        if(snap.documents.length<10) _edgeReached=true;
+        _snapshotTagMessageList.addAll(snap.documents);
+        _listTagMessageControllerSink.add(_snapshotTagMessageList);
+        _fetching=false;
+        _loadindControllerSink.add(_fetching);
+      });
+  }
+
+
+
+  void _onNewSnapshot(QuerySnapshot snapshot){
+    if(_snapshotTagMessageList.length!=0){
+      snapshot.documentChanges.forEach((DocumentChange doc){
+        final int t1=int.parse(doc.document.data["timeStamp"]);
+        final int t2=int.parse(_snapshotTagMessageList.elementAt(0).data["timeStamp"]);
+        final String id1 = doc.document.documentID;
+        final String id2 = _snapshotTagMessageList.elementAt(0).documentID;
+        print("new doc : "+doc.document.data["content"]+t1.toString()+id1);
+        print("lastDoc : "+_snapshotTagMessageList.elementAt(0).data["content"]+t2.toString()+id2);
+        if(doc.type==DocumentChangeType.added &&  t1 >= t2 && id1!=id2 ){
+            print("added!!!!!!!");
+            _snapshotTagMessageList.insert(0,doc.document);
+        }
+      });
+    }
+    if(_snapshotTagMessageList.length==0) {
+      _snapshotTagMessageList.addAll(snapshot.documents);
+      _lastDocFetched=snapshot.documents[snapshot.documents.length-1];
+    }
+    _listTagMessageControllerSink.add(_snapshotTagMessageList);
+  }
 
 
 
   BlocTagsPage(this._tag,keyGallery,keyChat) {
-    _listPage=[TagsGallery(_tag,key: keyGallery,),TagsChat(_tag,_snapshotTagMessageList,key:keyChat ,)];
+    _listPage=[TagsGallery(_tag,key: keyGallery,),TagsChat(_tag,this,key:keyChat)];
     numTabStream.listen(_onTabChange);
+    Firestore.instance.collection("Tags").document(_tag.id)
+      .collection("TagsMessage").orderBy("id",descending:true).limit(10).snapshots().listen(_onNewSnapshot);
+    _fetchMoreContentControllerStream.listen(_fetchMoreMessage);
   }
 
   void _onTabChange(int numTab){
@@ -52,6 +122,11 @@ class BlocTagsPage extends  BlocBase {
   void dispose() {
     _numTabController.close();
     _widgetPageController.close();
+    _listTagsPageController.close();
+    _listTagMessageController.close();
+    _fetchMoreContentController.close();
+    _listTagsPageControllerSink.close();
+    _loadindController.close();
     // TODO: implement dispose
   }
 
--- lib/pages/TagsPage/tags_chat.dart
+++ lib/pages/TagsPage/tags_chat.dart
@@ -1,5 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:tags/Bloc/bloc_provider.dart';
+import 'package:tags/Bloc/bloc_tags_page.dart';
+import 'package:tags/Event/events.dart';
 import 'package:tags/Models/tags.dart';
 import 'package:tags/UI/message_tile.dart';
 
@@ -8,9 +11,9 @@ class TagsChat extends StatefulWidget {
   //TODO: va peut être falloir le transformer en stful
 
   final Tags tag;
-  final Set<DocumentSnapshot> _snapshotList;
+  final BlocTagsPage _blocTagPage;
 
-  TagsChat(this.tag,this._snapshotList,{Key key}):super(key:key);
+  TagsChat(this.tag,this._blocTagPage,{Key key}):super(key:key);
 
   @override
   TagsChatState createState() {
@@ -21,31 +24,16 @@ class TagsChat extends StatefulWidget {
 class TagsChatState extends State<TagsChat>{
 
   ScrollController _scrollController;
-  DocumentSnapshot _lastDocFetched;
-  Stream<QuerySnapshot> _paginatedQuery;
-  List<DocumentSnapshot> _snapshotTagMessageList=List<DocumentSnapshot>();
-  List<DocumentSnapshot> temp=List<DocumentSnapshot>();
-  bool _edgeReached=false;
-  bool _fetching=false;
+  
+  
+  
   
 
   //TODO: faire un cache pour ne pas recharger les info déja afficher 
 
   void _fetchMoreMessage() async {
-    if(_edgeReached) return;
     if(_scrollController.position.pixels==_scrollController.position.maxScrollExtent){
-        Firestore.instance.collection("Tags").document(widget.tag.id)
-          .collection("TagsMessage").orderBy("id",descending:true)
-          .startAfter([_lastDocFetched.documentID]).limit(10).getDocuments().then((snap){
-              
-              setState(() {
-                _fetching=true;
-                print("-*-*-*-*-*-*-*-*-*-limit de document atteinte-*-*-*-*-*-*-*-*-*-*-*-*-*");
-                if(snap.documents.length>0) _lastDocFetched=snap.documents[snap.documents.length-1];
-                if(snap.documents.length<10) _edgeReached=true;
-                _snapshotTagMessageList.addAll(snap.documents);
-            });
-          });
+        widget._blocTagPage.fetchMoreContentControllerSink.add(FetchMoreEvent());
     }
   }
 
@@ -54,8 +42,6 @@ class TagsChatState extends State<TagsChat>{
     // TODO: implement initState
     super.initState();
     print("---------[initState tagsChat]-----------");
-    _paginatedQuery = Firestore.instance.collection("Tags").document(widget.tag.id)
-      .collection("TagsMessage").orderBy("id",descending:true).limit(10).snapshots();
     _scrollController = ScrollController();
     _scrollController.addListener(_fetchMoreMessage);
   }
@@ -63,57 +49,49 @@ class TagsChatState extends State<TagsChat>{
   
   @override
   Widget build(BuildContext context) {
-    String blabla="3";
-    
     return StreamBuilder(
-      stream: _paginatedQuery ,
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
-        if(!snapshot.hasData){
+      initialData: widget._blocTagPage.snapshotTagMessageList,
+      stream: widget._blocTagPage.listTagMessageControllerStream ,
+      builder: (BuildContext context, AsyncSnapshot<List<DocumentSnapshot>> listSnapshot){
+        if(!listSnapshot.hasData){
           return Center(
             child: CircularProgressIndicator(),
           );
         }
-        if (snapshot.data.documents.length==0) {
+        if (listSnapshot.data.length==0) {
             return Center(
               child: Text("ajoute un premier message !"),
             );
           }
-          
-          if(_snapshotTagMessageList.length!=0){
-              snapshot.data.documentChanges.forEach((DocumentChange doc){
-              final int t1=int.parse(doc.document.data["timeStamp"]);
-              final int t2=int.parse(_snapshotTagMessageList.elementAt(0).data["timeStamp"]);
-              final String id1 = doc.document.documentID;
-              final String id2 = _snapshotTagMessageList.elementAt(0).documentID;
-              print("new doc : "+doc.document.data["content"]+t1.toString()+id1);
-              print("lastDoc : "+_snapshotTagMessageList.elementAt(0).data["content"]+t2.toString()+id2);
-              if(doc.type==DocumentChangeType.added &&  t1 >= t2 && id1!=id2 ){
-                print("added!!!!!!!");
-                _snapshotTagMessageList.insert(0,doc.document);
-              }
-            });
-          }
-          if(_snapshotTagMessageList.length==0 /*&& !_fetching*/) {
-            _snapshotTagMessageList.addAll(snapshot.data.documents);
-            _lastDocFetched=snapshot.data.documents[snapshot.data.documents.length-1];
-          }
-          print(_snapshotTagMessageList);
-          
-        print("******[stb tagsChat] trigered********* "+snapshot.data.documents.length.toString());
-        return ListView.builder(
-          controller: _scrollController,
-          reverse: true,
-          itemCount: _snapshotTagMessageList.length,
-          addAutomaticKeepAlives: true,
-          itemBuilder: (BuildContext context, int index) {
-            _fetching=false;
+        print("******[stb tagsChat] trigered********* "+listSnapshot.data.length.toString());
+        return StreamBuilder(
+          stream: widget._blocTagPage.loadindControllerStream ,
+          initialData: false ,
+          builder: (BuildContext context, AsyncSnapshot snapshot){
             return Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                MessageTile.fromDocumentSnapshot(_snapshotTagMessageList[index]),
-                Divider()
+                snapshot.data?  CircularProgressIndicator() : Container(width: 0.0,height: 0.0,),
+                Expanded(
+                  child: ListView.builder(
+                  controller: _scrollController,
+                  padding: EdgeInsets.only(top: 70.0),
+                  reverse: true,
+                  itemCount: listSnapshot.data.length,
+                  addAutomaticKeepAlives: true,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Column(
+                      children: <Widget>[
+                        MessageTile.fromDocumentSnapshot(listSnapshot.data[index]),
+                        Divider()
+                      ],
+                    );
+                  }
+                ),
+                )
               ],
             );
-          }
+          },
         );
       }
     );
