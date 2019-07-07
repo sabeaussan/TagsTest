a9c429a18daaeb16d5b98e8ddce33459ffb6381b
sabeaussan <sambeda86@gmail.com>
2019-07-07 22:39:59 +0200

    ajout fav page + modif UI

---
+++ lib/pages/FavPage/perso_fav_page.dart
@@ -0,0 +1,115 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:tags/Bloc/bloc_provider.dart';
+import 'package:tags/Bloc/main_bloc.dart';
+import 'package:tags/Event/events.dart';
+import 'package:tags/Models/tags.dart';
+import 'package:tags/UI/tags_tile.dart';
+
+
+//TODO: peut etre passé en stateless
+
+
+class PersoFavPage extends StatefulWidget {
+  _PersoFavPageState createState() => _PersoFavPageState();
+}
+
+class _PersoFavPageState extends State<PersoFavPage> {
+
+  MainBloc _mainBloc;
+  Future<List<Tags>> futureUserFavMark;
+  ScrollController _scrollController;
+  double lastExtent=0.0;
+  int _fetchIndex=0;
+
+  void _fetchMoreFavTags() async {
+    if(_scrollController.position.pixels==_scrollController.position.maxScrollExtent){
+      if(lastExtent!=_scrollController.position.maxScrollExtent){
+        print("--------------------FetchMoreFavTagsEvent triggered----------------");
+        print("position : "+_scrollController.position.pixels.toString());
+        print("maxExtent : "+_scrollController.position.pixels.toString());
+        print("lastExtent : "+lastExtent.toString());
+        lastExtent = _scrollController.position.maxScrollExtent;
+        _mainBloc.fetchMoreFavTagControllerSink.add(FetchMoreFavTagsEvent(_fetchIndex));
+        _fetchIndex=_fetchIndex+2;
+      } 
+    }
+  }
+  
+
+  Widget _buildLoadingIndicator(bool isLoading){
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Center(
+        child: Opacity(
+          opacity: isLoading ? 1.0 : 0.0,
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildListPost(List<Tags> list, bool isLoading){
+    List<Widget> _widgetList =list.map((Tags tag){
+      final TagsTile tile = TagsTile(tag, "", false,true,true);
+      return Padding(
+        padding: EdgeInsets.all(0.0),
+        child: tile,
+      );
+    }).toList();
+    _widgetList.add(_buildLoadingIndicator(isLoading));
+    return _widgetList;
+  }
+
+  String setDistanceLabel(Tags tag){
+    int dist = tag.distance.toInt();
+    int r = dist%10;
+    dist = dist - r +10;
+    return dist.toString();
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _mainBloc = BlocProvider.of<MainBloc>(context);
+    _scrollController = ScrollController();
+    _mainBloc.setFavTagsEdgeReached(false);
+    _scrollController.addListener(_fetchMoreFavTags);
+    _mainBloc.fetchMoreFavTagControllerSink.add(FetchMoreFavTagsEvent(_fetchIndex));
+    _fetchIndex=_fetchIndex+2;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      //initialData: widget._blocTagPage.snapshotTagPostList,
+      stream: _mainBloc.listFavTagControllerStream,
+      builder: (BuildContext context, AsyncSnapshot<List<Tags>> listSnapshot){
+        if(!listSnapshot.hasData){
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (listSnapshot.data.length==0) {
+            return Center(
+              child: Text("Vous n'avez pas enregistrer de favori"),
+            );
+        }
+          return StreamBuilder(
+            stream: _mainBloc.loadingFavTagsControllerStream,
+            initialData: false,
+            builder: (BuildContext context, AsyncSnapshot snapshot){
+              return ListView(
+                controller: _scrollController,
+                children: _buildListPost(listSnapshot.data,snapshot.data),
+            
+              );
+            },
+          );
+        
+      }
+      
+    );
+  }
+}
---
+++ lib/pages/FavPage/popular_fav_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:tags/Bloc/bloc_provider.dart';
+import 'package:tags/Bloc/main_bloc.dart';
+import 'package:tags/Models/tags.dart';
+import 'package:tags/UI/tags_tile.dart';
+
+class PopularFavPage extends StatefulWidget {
+
+  _PopularFavPageState createState() => _PopularFavPageState();
+}
+
+class _PopularFavPageState extends State<PopularFavPage> {
+  MainBloc _mainBloc;
+  String _distanceLabel;
+    //ajouter un model post
+
+  String setDistanceLabel(Tags tag){
+    int dist = tag.distance.toInt();
+    int r = dist%10;
+    dist = dist - r +10;
+    return dist.toString();
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _mainBloc = BlocProvider.of<MainBloc>(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      initialData: _mainBloc.mostPopularTags,
+      stream: _mainBloc.listPopularTagsControllerStream,
+      builder: (BuildContext context, AsyncSnapshot<List<Tags>> snapshot){
+        if(!snapshot.hasData){
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (snapshot.data.length==0) {
+            return Center(
+              child: Text("Rien a afficher"),
+            );
+          }
+          return ListView.builder(
+              itemCount: snapshot.data.length ,
+              physics: const AlwaysScrollableScrollPhysics(),
+              itemBuilder: (BuildContext context, int index){
+                _distanceLabel=setDistanceLabel(snapshot.data[index]);
+                return TagsTile(snapshot.data[index], _distanceLabel, false,false,true);
+              }
+          ); 
+      }
+    );
+  }
+}
